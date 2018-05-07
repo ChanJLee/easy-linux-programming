7b504dcf62781c5468670523a493efc8bd693512
ChanJLee <1355425625@qq.com>
2018-05-07 22:19:32 +0800

    add-readme

--- read-args/README.md
+++ read-args/README.md
@@ -1,1 +1,19 @@
 read arguments
+
+# EXAMPLE
+
+```shell
+make build
+./main -v -f
+./main -f ff -l
+```
+
+# RESULE
+
+```
+unkown argument: 118
+need argument
+optind value: 4
+
+f option value: ff
+```
--- read-args/main.cpp
+++ read-args/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    std::cout << "hello world" << std::endl;
+    int opt;
+    while ((opt = getopt(argc, argv, ":lf:")) != -1)
+    {
+        switch (opt)
+        {
+        case ':':
+            std::cout << "need argument" << std::endl;
+            break;
+        case '?':
+            std::cout << "unkown argument: " << optopt << std::endl;
+            break;
+        case 'f':
+            std::cout << "f option value: " << optarg << std::endl;
+            break;
+        case 'l':
+            std::cout << "enable l" << std::endl;
+            break;
+        }
+    }
+
+    std::cout << "optind value: " << optind << std::endl;
+    for (; optind < argc; ++optind)
+    {
+        std::cout << "index: " << optind << " argument: " << argv[optind] << std::endl;
+    }
     return 0;
 }

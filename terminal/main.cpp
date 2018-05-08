e6a901b43da33d1210f24ce4dff5e6f094c8cd4a
ChanJLee <1355425625@qq.com>
2018-05-08 22:12:04 +0800

    add-tty

--- terminal/main.cpp
+++ terminal/main.cpp
@@ -17,7 +17,8 @@ int main(int argc, char* argv[])
     // true is tty
     // fale is redirect
     std::cout << "is terminal: " << isatty(fileno(stdout)) << std::endl;
-    show_dialog_std();
+    //show_dialog_std();
+    show_dialog_tty();
     return 0;
 }
 
@@ -36,11 +37,12 @@ void show_dialog_std()
         while(*menu_item) {
             if (*menu_item[0] == o) {
                 std::cout << "you select: " << *menu_item << std::endl;
+                break;
             }
             ++menu_item;
         }
 
-        if (menu_item == NULL) {
+        if (*menu_item == NULL) {
             std::cerr << "invalid argument " << o << std::endl;
         }
 
@@ -51,7 +53,51 @@ void show_dialog_std()
     } while(o != 'q');
 }
 
+#include <cstdlib>
+
 void show_dialog_tty()
 {
-    
+    FILE* in = fopen("/dev/tty", "r");
+    if (in == NULL) {
+        std::cerr << "open tty failed" << std::endl;
+        return;
+    }
+
+    FILE* out = fopen("/dev/tty", "w");
+    if (out == NULL) {
+        fclose(in);
+        std::cerr << "open tty failed" << std::endl;
+        return;
+    }
+
+    char o;
+    do {
+        const char** menu_item = menu;
+        while(*menu_item) {
+            fprintf(out, "%s\n", *menu_item);
+            ++menu_item;
+        }
+
+        o = fgetc(in);
+        menu_item = menu;
+        while(*menu_item) {
+            if (*menu_item[0] == o) {
+                fprintf(out, "ou select: %s\n", *menu_item);
+                break;
+            }
+            ++menu_item;
+        }
+
+        if (*menu_item == NULL) {
+            std::cerr << "invalid argument " << o << std::endl;
+        }
+
+        char x = fgetc(in);
+        while (x != '\n') {
+            x = fgetc(in);
+        }
+    } while(o != 'q');
+
+    fclose(in);
+    fclose(out);
 }

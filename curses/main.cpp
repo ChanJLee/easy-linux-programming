bd76d9cfac9a6bdb2ea33085e488e64fe8e6d1a1
ChanJLee <1355425625@qq.com>
2018-05-10 23:23:25 +0800

    add curses api

--- curses/main.cpp
+++ curses/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <curses.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    initscr();
-    move(10, 10);
+    WINDOW *win = initscr();
+
+    move(0, 10);
     printw("%s", "hello world");
     refresh();
+
+    move(1, 10);
+    attron(A_BOLD);
+    printw("%s", "hello world");
+    attroff(A_BOLD);
+    refresh();
+
+    refresh();
+    sleep(2);
+    endwin();
     return 0;
 }

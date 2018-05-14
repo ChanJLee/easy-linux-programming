210c6362bf979268fbcb317e146f6260fe4471f1
ChanJLee <1355425625@qq.com>
2018-05-14 22:28:40 +0800

    add-comment

--- env/main.cpp
+++ env/main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     const char *linux_pro_env = getenv("linux_pro_env");
-    if (strlen(linux_pro_env))
+    if (linux_pro_env && strlen(linux_pro_env))
     {
         std::cout << "linux pro env" << linux_pro_env << std::endl;
     }
 
     const char* env = "linux_pro_env=hello_world";
-    char* env_dy = (char*) malloc(strlen(env) + 1);
-    memset(env_dy, 0, strlen(env) + 1);
-    memcpy(env_dy, env, strlen(env));
+    const size_t len = strlen(env);
+
+    // free on exit
+    char* env_dy = (char*) malloc(len + 1);
+    memset(env_dy, 0, len + 1);
+    memcpy(env_dy, env, len);
 
     if (putenv(env_dy))
     {
         std::cerr << "put linux_pro_env failed" << std::endl;
         return -2;
     }
+
+    std::cout << getenv("linux_pro_env") << std::endl;
 
     return 0;
 }

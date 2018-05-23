8b0360f961ea2a751293d31c50b0d36277b4cf8c
ChanJLee <1355425625@qq.com>
2018-05-23 23:20:54 +0800

    add process wait

--- fork/README.md
+++ fork/README.md
@@ -10,7 +10,15 @@ make
 # RESULT
 
 ```
-main pid: 6994
-I am parent, pid: 6994 fork: 6995
-I am child, pid: 6995 fork: 0
+main pid: 8036
+I am parent, pid: 8036 fork: 8037
+I am parent, pid: 8036 fork: 8037
+wait for child
+I am child, pid: 8037 fork: 0
+I am child, pid: 8037 fork: 0
+I am child, pid: 8037 fork: 0
+I am child, pid: 8037 fork: 0
+I am child, pid: 8037 fork: 0
+catch child, pid: 8037
+child exit code is 37
 ```
--- fork/main.cpp
+++ fork/main.cpp
@@ -1,22 +1,54 @@
 #include <iostream>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <string>
+#include <sstream>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     std::cout << "main pid: " << getpid() << std::endl;
+    int exit_code = 0;
     pid_t id = fork();
-    switch(id) {
-        case -1:
-            std::cerr << "fork failed" << std::endl;
-            break;
-        case 0:
-            std::cout << "I am child, pid: " << getpid() << " fork: " << id << std::endl;
-            break;
-        default:
-            std::cout << "I am parent, pid: " << getpid() << " fork: " << id << std::endl;
-            break;
+    int i = 5;
+    std::ostringstream os;
+
+    switch (id)
+    {
+    case -1:
+        std::cerr << "fork failed" << std::endl;
+        break;
+    case 0:
+        exit_code = 37;
+        os << "I am child, pid: " << getpid() << " fork: " << id;
+        break;
+    default:
+        i = 2;
+        os << "I am parent, pid: " << getpid() << " fork: " << id;
+        break;
     }
 
-    return 0;
+    const std::string &msg = os.str();
+    for (; i > 0; --i)
+    {
+        std::cout << msg << std::endl;
+    }
+
+    if (id)
+    {
+        std::cout << "wait for child" << std::endl;
+        int mask;
+        pid_t child = wait(&mask);
+        std::cout << "catch child, pid: " << child << std::endl;
+        if (WIFEXITED(mask))
+        {
+            std::cout << "child exit code is " << WEXITSTATUS(mask) << std::endl;
+        } 
+        else
+        {
+            std::cout << "child abort" << std::endl;
+        }
+    }
+
+    return exit_code;
 }

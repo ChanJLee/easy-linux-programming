c0ddb595c2bf163c16245897733d36d12afbc0e6
ChanJLee <1355425625@qq.com>
2018-05-27 21:39:58 +0800

    sync

--- sigaction/main.cpp
+++ sigaction/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <unistd.h>
+#include <signal.h>
+
+void handler(int sig) 
+{
+    std::cout << "recv sig " << sig << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
-    std::cout << "hello world" << std::endl;
+    struct sigaction action;
+    
+    action.sa_handler = &handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+
+    sigaction(SIGINT, &action, NULL);
+
+    for (int i = 0;; ++i)
+    {
+        sleep(1);
+        std::cout << "sleep " << i << " s" << std::endl;
+    }
+
     return 0;
 }
